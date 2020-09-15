0ebc448ebf19a99f0c7fde273c90f8a37ca29ecb
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-15 13:25:21 -0600

    Create instance variables for item class

--- lib/item.rb
+++ lib/item.rb
@@ -1,4 +1,11 @@
 class Item
+  attr_reader :name, :price
   def initialize(data_hash)
+    @name = data_hash[:name]
+    @price = convert_price_to_integer(data_hash[:price])
+  end
+
+  def convert_price_to_integer(price)
+    price.gsub(/[$]/, '').to_f
   end
 end
