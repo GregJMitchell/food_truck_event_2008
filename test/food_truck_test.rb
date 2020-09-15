f26daeeeba6625a51679f2c41b7259bf0aca9e49
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-15 13:40:37 -0600

    Create test for stock food method

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -23,4 +23,12 @@ class FoodTruckTest <Minitest::Test
   def test_it_can_check_stock
     assert_equal 0, @food_truck.check_stock(@item1)
   end
+
+  def test_it_can_add_stock
+    hash = {}
+    expected = hash[@item1] = 30
+    @food_truck.stock(@item1, 30)
+
+    assert_equal expected, @food_truck.inventory
+  end
 end
