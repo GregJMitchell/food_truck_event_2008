36f4756d291180ce1e7fdf22b25d055d0dac65a3
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-15 13:34:18 -0600

    Add setup method for test and create test for check stock method

--- test/food_truck_test.rb
+++ test/food_truck_test.rb
@@ -4,16 +4,23 @@ require './lib/item'
 require './lib/food_truck'
 
 class FoodTruckTest <Minitest::Test
+  def setup
+    @food_truck = FoodTruck.new("Rocky Mountain Pies")
+    @item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    @item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+  end
+
   def test_it_exists
-    food_truck = FoodTruck.new("Rocky Mountain Pies")
-    assert_instance_of FoodTruck, food_truck
+    assert_instance_of FoodTruck, @food_truck
   end
 
   def test_it_has_readable_attributes
-    food_truck = FoodTruck.new("Rocky Mountain Pies")
-
     expected = {}
-    assert_equal "Rocky Mountain Pies", food_truck.name
-    assert_equal expected, food_truck.inventory
+    assert_equal "Rocky Mountain Pies", @food_truck.name
+    assert_equal expected, @food_truck.inventory
+  end
+
+  def test_it_can_check_stock
+    assert_equal 0, @food_truck.check_stock(@item1)
   end
 end
